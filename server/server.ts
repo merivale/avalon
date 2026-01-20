4de0bf22dcee7ad4ca3e63220a11ee8c0f9c8ffc
Amyas Merivale <merivale@gmail.com>
2026-01-20 15:41:15 +0100

    Remove duplicate heartbeat

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,6 @@ import fixtures from "@/server/persistence/fixtures.ts";
 export default {
   start(port = 0) {
     const onListen = () => {
-      setInterval(sendHeartbeat, 30000);
       console.log(`Server running on http://localhost:${port}`);
     };
     Deno.serve({ port, onListen }, handler);
